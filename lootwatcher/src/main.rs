9ee70350dacfba380a44b5b7a10f8c94a73768c3
Ferocity <christian.fries123@gmail.com>
2021-04-02 12:10:34 +0200

    lootwatcher requests update perodically

--- lootwatcher/src/main.rs
+++ lootwatcher/src/main.rs
@@ -4,25 +4,26 @@ use tokio::net::TcpStream;
 use tokio_serde::formats::*;
 use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
 use std::thread::sleep;
-use std::time::Duration;
 use futures::executor::block_on;
-
+use std::time::Duration;
+use tokio::time;
 
 #[tokio::main]
 pub async fn main() {
     let mut socket = TcpStream::connect("127.0.0.1:8080").await.unwrap();
-
     let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
     let mut serialized =
         tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::default());
+    let mut interval = time::interval(Duration::from_secs(1));
+    loop {
 
-    serialized
-        .send(json!(
-    {
-        "action": "update"
+
+        serialized.send(json!(
+        {
+            "action": "update"
+        }
+        )).await.unwrap();
+
+        interval.tick().await;
     }
-    ))
-        .await
-        .unwrap();
-
 }
