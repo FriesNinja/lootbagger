6e190c1ad8433f0270bd38cbb50ba320e2ca1845
Christian Fries <christian.fries123@gmail.com>
2021-04-20 20:03:11 +0200

    prepare core for project initialization

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,29 @@
 use log::{info, trace, warn, error};
+use serde::{Serialize, Deserialize};
 
-pub fn core()
-{
-    info!("Hello core!");
+#[derive(Serialize, Deserialize)]
+struct Project {
+    name: String
 }
+
 
 #[cfg(test)]
 mod tests {
+    use crate::Project;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn deserialize_project() {
+        let project: Project = toml::from_str(r#"
+            name = "Test"
+            "#).unwrap();
+            assert_eq!(project.name, "Test");
+    }
+
+    #[test]
+    fn serialize_project() {
+        let project = Project { name: "Test".to_string() };
+        let toml = toml::to_string(&project).unwrap();
+        assert_eq!(toml, r#"name = "Test"
+"#);
     }
 }
